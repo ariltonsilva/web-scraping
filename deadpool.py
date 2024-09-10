d33830cc115af16c48bdaee427e6d28ff03fdf09
arilton oliveira <112397941+ariltonsilva@users.noreply.github.com>
2024-09-10 20:21:08 -0300

    Update deadpool.py

--- deadpool.py
+++ deadpool.py
@@ -2,7 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 # Configura o WebDriver
@@ -15,14 +17,18 @@ driver.get('https://www.omelete.com.br/')
 search_button = driver.find_element(By.CLASS_NAME, 'h-menu__search')
 search_button.click()
 
-# Encontra o campo de busca e realiza a busca por "Deadpool"
-
-search_box = driver.find_element(By.NAME, 'q')
-search_box.send_keys('Deadpool')
-search_box.send_keys(Keys.RETURN)
+# Espera até que o campo de busca seja visível e interativo
+try:
+    search_box = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.NAME, 'q'))
+    )
+    search_box.send_keys('Deadpool')
+    search_box.send_keys(Keys.RETURN)
+except TimeoutException:
+    print("O campo de busca não ficou interativo a tempo.")
 
 # Espera a página carregar os resultados
-time.sleep(15)
+time.sleep(10)
 
 # Captura os títulos e datas das notícias
 news_items = driver.find_elements(By.CLASS_NAME, 'mark')
